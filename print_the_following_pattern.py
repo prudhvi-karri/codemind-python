78d6b98f28ebd1926329f29931b589b295803d7a
prudhvi-karri <97424288+prudhvi-karri@users.noreply.github.com>
2022-05-05 11:43:58 +0530

    a=int(input()) for i in range(65,a+65):     for j in range(1,a+1):             print(chr(i),end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,5 @@
 a=int(input())
-for i in range(1,a+1):
-    for j in range(1,i+1):
-        print(j,end='')
+for i in range(65,a+65):
+    for j in range(1,a+1):
+            print(chr(i),end=' ')
     print()
