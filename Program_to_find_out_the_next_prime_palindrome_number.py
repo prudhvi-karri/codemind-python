5da5ded0e500480cfcd4ea01f0a21094c88fd544
prudhvi-karri <97424288+prudhvi-karri@users.noreply.github.com>
2022-06-09 16:34:20 +0530

    def prime(i):     c=0     for j in range(2,(i//2)+1):         if i%j==0:             c+=1     if c==0:         return 1 def palindrome(i):     temp=i     rev=0     while(i):         d=i%10         rev=rev*10+d         i=i//10     if temp==rev:         return 1     else:         return 0
    n=int(input())
    for i in range(n+1,100000):
        if prime(i)==1 and palindrome(i)==1:
            print(i)
            break

--- Program_to_find_out_the_next_prime_palindrome_number.py
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -1,24 +1,24 @@
-def is_prime(m):
-    if m==0:
-        return 0
-    for j in range(2,int(m**(0.5))+1):
-        if m%j==0:
-            return 0
-    return 1
-def is_pali(o):
-    t=o
+def prime(i):
+    c=0
+    for j in range(2,(i//2)+1):
+        if i%j==0:
+            c+=1
+    if c==0:
+        return 1
+def palindrome(i):
+    temp=i
     rev=0
-    while o!=0:
-        r=o%10
-        rev=rev*10+r
-        o//=10
-    if rev==t:
+    while(i):
+        d=i%10
+        rev=rev*10+d
+        i=i//10
+    if temp==rev:
         return 1
     else:
         return 0
+    
 n=int(input())
-for i in range(1,10000):
-    if is_pali(n+i):
-        if is_prime(n+i):
-            print(n+i)
-            break
+for i in range(n+1,100000):
+    if prime(i)==1 and palindrome(i)==1:
+        print(i)
+        break
